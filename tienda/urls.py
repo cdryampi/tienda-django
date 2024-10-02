063eccc1d70eb6e0a81fca117060d205593632e1
cdryampi <cdryampi@gmail.com>
2024-10-03 01:48:31 +0200

    añadiendo los ficheros estaticos

--- tienda/urls.py
+++ tienda/urls.py
@@ -14,12 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
-
     path('admin/', admin.site.urls),
-    path('', include('core.urls'))
+    path('', include('core.urls')),
 ]
+
+# Siempre servir archivos estáticos, tanto en desarrollo como en producción
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
