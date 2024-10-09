06c29285e5e06e104a00633b25b35d7b2230ed89
cdryampi <cdryampi@gmail.com>
2024-10-09 03:22:23 +0200

    fix: ajustar creación del grupo 'Cliente' y registro de señales en apps.py para evitar errores de inicialización.

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,9 +1,9 @@
 from django.apps import AppConfig
-from django.contrib.auth.models import Group
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
     def ready(self):
         import accounts.signals
+        from django.contrib.auth.models import Group
         Group.objects.get_or_create(name='cliente')
